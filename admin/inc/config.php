85edca52a53d19f96cb1d9f8e91631f302b5eadb
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2024-03-08 08:20:31 +0200

    added files via upload

---
+++ admin/inc/config.php
@@ -0,0 +1,35 @@
+<?php
+// Error Reporting Turn On
+ini_set('error_reporting', E_ALL);
+
+// Setting up the time zone
+date_default_timezone_set('America/Los_Angeles');
+
+// Host Name
+$dbhost = 'localhost';
+
+// Database Name
+$dbname = 'ecommerceweb';
+
+// Database Username
+$dbuser = 'root';
+
+// Database Password
+$dbpass = 'Mysql@2024';
+
+// Defining base url
+define("BASE_URL", "");
+
+//port number
+$port = '3307';
+
+// Getting Admin url
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+try {
+	$pdo = new PDO("mysql:host={$dbhost};port={$port};dbname={$dbname}", $dbuser, $dbpass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+	echo "Connection error :" . $exception->getMessage();
+}
