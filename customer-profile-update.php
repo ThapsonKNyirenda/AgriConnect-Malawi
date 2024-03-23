3ab52347be3e61d334b6d20fbb9aa10d93615e70
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2024-03-23 16:44:38 +0200

    added files via upload

--- customer-profile-update.php
+++ customer-profile-update.php
@@ -22,9 +22,36 @@ if (isset($_POST['form1'])) {
 
     $valid = 1;
 
+    echo $_POST['cust_name'];
+    echo $_POST['cust_email'];
+    echo $_POST['cust_phone'];
+    echo $_POST['cust_address'];
+    echo $_POST['farm_type'];
+    echo $_POST['acc_type'];
+    echo $_POST['cust_city'];
+    die;
+
     if(empty($_POST['cust_name'])) {
         $valid = 0;
         $error_message .= LANG_VALUE_123."<br>";
+    }
+
+    if(empty($_POST['cust_email'])) {
+        $valid = 0;
+        $error_message .= LANG_VALUE_131."<br>";
+    } else {
+        if (filter_var($_POST['cust_email'], FILTER_VALIDATE_EMAIL) === false) {
+            $valid = 0;
+            $error_message .= LANG_VALUE_134."<br>";
+        } else {
+            $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_email=?");
+            $statement->execute(array($_POST['cust_email']));
+            $total = $statement->rowCount();                            
+            if($total) {
+                $valid = 0;
+                $error_message .= LANG_VALUE_147."<br>";
+            }
+        }
     }
 
     if(empty($_POST['cust_phone'])) {
@@ -37,9 +64,9 @@ if (isset($_POST['form1'])) {
         $error_message .= LANG_VALUE_125."<br>";
     }
 
-    if(empty($_POST['cust_country'])) {
+    if(empty($_POST['farm_type'])) {
         $valid = 0;
-        $error_message .= LANG_VALUE_126."<br>";
+        $error_message .= "Please Provide Farming type"."<br>";
     }
 
     if(empty($_POST['cust_city'])) {
@@ -47,14 +74,26 @@ if (isset($_POST['form1'])) {
         $error_message .= LANG_VALUE_127."<br>";
     }
 
-    if(empty($_POST['cust_state'])) {
+    if(empty($_POST['acc_type'])) {
         $valid = 0;
-        $error_message .= LANG_VALUE_128."<br>";
+        $error_message .= "Please Specify either customer or vendor"."<br>";
     }
 
-    if(empty($_POST['cust_zip'])) {
+    // if(empty($_POST['cust_zip'])) {
+    //     $valid = 0;
+    //     $error_message .= LANG_VALUE_129."<br>";
+    // }
+
+    if( empty($_POST['cust_password']) || empty($_POST['cust_re_password']) ) {
         $valid = 0;
-        $error_message .= LANG_VALUE_129."<br>";
+        $error_message .= LANG_VALUE_138."<br>";
+    }
+
+    if( !empty($_POST['cust_password']) && !empty($_POST['cust_re_password']) ) {
+        if($_POST['cust_password'] != $_POST['cust_re_password']) {
+            $valid = 0;
+            $error_message .= LANG_VALUE_139."<br>";
+        }
     }
 
     if($valid == 1) {
@@ -63,26 +102,25 @@ if (isset($_POST['form1'])) {
         $statement = $pdo->prepare("UPDATE tbl_customer SET cust_name=?, cust_cname=?, cust_phone=?, cust_country=?, cust_address=?, cust_city=?, cust_state=?, cust_zip=? WHERE cust_id=?");
         $statement->execute(array(
                     strip_tags($_POST['cust_name']),
-                    strip_tags($_POST['cust_cname']),
+                    strip_tags($_POST['farm_type']),
                     strip_tags($_POST['cust_phone']),
-                    strip_tags($_POST['cust_country']),
+                    '',
                     strip_tags($_POST['cust_address']),
                     strip_tags($_POST['cust_city']),
-                    strip_tags($_POST['cust_state']),
-                    strip_tags($_POST['cust_zip']),
+                    '',
+                    '',
+                    strip_tags($_POST['acc_type']),
                     $_SESSION['customer']['cust_id']
                 ));  
        
         $success_message = LANG_VALUE_130;
 
         $_SESSION['customer']['cust_name'] = $_POST['cust_name'];
-        $_SESSION['customer']['cust_cname'] = $_POST['cust_cname'];
+        $_SESSION['customer']['farm_type'] = $_POST['farm_type'];
         $_SESSION['customer']['cust_phone'] = $_POST['cust_phone'];
-        $_SESSION['customer']['cust_country'] = $_POST['cust_country'];
+        $_SESSION['customer']['acc_type'] = $_POST['acc_type'];
         $_SESSION['customer']['cust_address'] = $_POST['cust_address'];
         $_SESSION['customer']['cust_city'] = $_POST['cust_city'];
-        $_SESSION['customer']['cust_state'] = $_POST['cust_state'];
-        $_SESSION['customer']['cust_zip'] = $_POST['cust_zip'];
     }
 }
 ?>
@@ -114,8 +152,8 @@ if (isset($_POST['form1'])) {
                                 <input type="text" class="form-control" name="cust_name" value="<?php echo $_SESSION['customer']['cust_name']; ?>">
                             </div>
                             <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_103; ?></label>
-                                <input type="text" class="form-control" name="cust_cname" value="<?php echo $_SESSION['customer']['cust_cname']; ?>">
+                                <label for="">Farming type</label>
+                                <input type="text" class="form-control" name="cust_cname" value="<?php echo $_SESSION['customer']['farm_type']; ?>">
                             </div>
                             <div class="col-md-6 form-group">
                                 <label for=""><?php echo LANG_VALUE_94; ?> *</label>
@@ -130,33 +168,18 @@ if (isset($_POST['form1'])) {
                                 <textarea name="cust_address" class="form-control" cols="30" rows="10" style="height:70px;"><?php echo $_SESSION['customer']['cust_address']; ?></textarea>
                             </div>
                             <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_106; ?> *</label>
-                                <select name="cust_country" class="form-control">
-                                <?php
-                                $statement = $pdo->prepare("SELECT * FROM tbl_country ORDER BY country_name ASC");
-                                $statement->execute();
-                                $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-                                foreach ($result as $row) {
-                                    ?>
-                                    <option value="<?php echo $row['country_id']; ?>" <?php if($row['country_id'] == $_SESSION['customer']['cust_country']) {echo 'selected';} ?>><?php echo $row['country_name']; ?></option>
-                                    <?php
-                                }
-                                ?>
-                                </select>                                    
-                            </div>
+                                    <label for="">Register as *</label>
+                                    <select name="acc_type" class="form-control select2">
+                                        <option value="customer">Customer</option>
+                                        <option value="vendor">Vendor</option>
+                                    </select>                                    
+                                </div>
                             
                             <div class="col-md-6 form-group">
                                 <label for=""><?php echo LANG_VALUE_107; ?> *</label>
                                 <input type="text" class="form-control" name="cust_city" value="<?php echo $_SESSION['customer']['cust_city']; ?>">
                             </div>
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_108; ?> *</label>
-                                <input type="text" class="form-control" name="cust_state" value="<?php echo $_SESSION['customer']['cust_state']; ?>">
-                            </div>
-                            <div class="col-md-6 form-group">
-                                <label for=""><?php echo LANG_VALUE_109; ?> *</label>
-                                <input type="text" class="form-control" name="cust_zip" value="<?php echo $_SESSION['customer']['cust_zip']; ?>">
-                            </div>
+                            
                         </div>
                         <input type="submit" class="btn btn-primary" value="<?php echo LANG_VALUE_5; ?>" name="form1">
                     </form>
